5f01dd188746eca8ca975fc90d4da339a434ebae
Jose Obeso <jobeso@irtp.gob.pe>
2025-03-09 16:33:22 -0500

    Agregar archivos de proyecto.

---
+++ EnviarCorreo/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+
+
+namespace EnviarCorreo
+{
+    class Program
+    {
+
+        public static event EventHandler EventoOcurrido;
+
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Enviar Correo");
+
+            EventoOcurrido += OnEventoOcurrido;
+
+            // Simular el evento
+            EventoOcurrido?.Invoke(null, EventArgs.Empty);
+
+
+
+
+
+        }
+
+
+        private static void OnEventoOcurrido(object sender, EventArgs e)
+        {
+            EmailSender emailSender = new EmailSender();
+            emailSender.SendEmail("jobeso@elpiquero.com", "Bienvenidos al Equipo IRTP", "Bienvenido");
+           
+        }
+
+        public class EmailSender
+        {
+
+             
+            private string smtpServer = "smtp.gmail.com";
+            private int smtpPort = 587; // o el puerto que uses
+            
+            private string smtpUser = "irtpbot@irtp.gob.pe";
+            private string smtpPass = "Dev#03$March#irtp";
+
+            public void SendEmail(string toEmail, string subject, string body)
+            {
+                try
+                {
+                    MailMessage mail = new MailMessage();
+                    SmtpClient smtpClient = new SmtpClient(smtpServer);
+
+                    mail.From = new MailAddress(smtpUser);
+                    mail.To.Add(toEmail);
+                    mail.Subject = subject;
+                    mail.Body = CuerpoMensaje();
+
+                    smtpClient.Port = smtpPort;
+                    smtpClient.Credentials = new NetworkCredential(smtpUser, smtpPass);
+                    smtpClient.EnableSsl = true;
+
+                    smtpClient.Send(mail);
+                    Console.WriteLine("Correo enviado exitosamente.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error al enviar el correo: " + ex.Message);
+                    Console.ReadKey();
+                }
+            }
+
+
+            public string CuerpoMensaje()
+            {
+                string m1 = "Bienvenidos al IRTP<br>";
+                m1 += "El equipo de desarrollo, esta conformado por <br>";
+                m1 += "------------------<br>";
+                m1 += " Gracias";
+
+                return m1;
+
+
+            }
+
+
+
+        }
+
+
+
+
+    }
+}
